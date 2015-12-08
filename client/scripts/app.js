76a5a42f14987c3b852399195288804a17d65b16
strangeInference <tesla.phipps@gmail.com>
2015-12-07 20:23:22 -0800

    refactored code to pass Spec

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,107 +1,208 @@
 // YOUR CODE HERE:
-$(document).ready(function(){
-  var rooms = {};
-  var currentRoom = "default";
-  var getMessages = function(){
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      success: function (data) {
-        console.log(data);
-        for(var i = 0; i < data.results.length; i++){
-          var item = data.results[i];
-
-          rooms[item.roomname] = item.roomname;
-          
-          var $post = $('<div></div>');
-          $post.addClass('post');
-          
-          var $username = $('<span></span>');
-          $username.text(item.username);
-          $username.addClass('username');
-          $username.appendTo($post);
-          
-          var $time = $('<span></span>');
-          $time.text(item.createdAt); 
-          $time.addClass('time');
-          $time.appendTo($post);
-          
-          var $message = $("<p></p>");
-          $message.addClass('message');
-          $message.text(item.text);
-          $message.appendTo($post);
-          
-          $post.appendTo($("#chats"));
-        }
-
-        // remove rooms from datalist
-        $('.room').remove();
-
-        // add rooms to datalist
-        for(var room in rooms){
-          var $option = $('<option>');
-          $option.attr('value', room);
-          $option.addClass('room');
-          $option.appendTo($('datalist#roomList'));
-        }
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  };
-  $('#new').on('click', function(){
-    // select all posts and remove them
-    $('.post').remove();
-    getMessages();
+var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.init = function(){
+  $(".username").on('click',function(){
+    console.log("added friend");
+    app.addFriend();
   });
 
-  $('#myRoom').on('input',function(e){
-    currentRoom = $(this).val();
-    // remove rooms from datalist
-    console.log(currentRoom);
-    $('.room').remove();
+  
+  $('#send .submit').on('submit',function(){
+    console.log("sumbitted");
+    app.handleSubmit();
+  });
+};
 
-    // add rooms to datalist
-    for(var room in rooms){
-      var $option = $('<option>');
-      $option.attr('value', room);
-      $option.addClass('room');
-      $option.appendTo($('datalist#roomList'));
+app.send = function(ourPost){
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(ourPost),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
     }
   });
+};
 
-  var postMessage = function(message){
-    var ourPost = {
-      username: window.location.search.slice(10),
-      text: message,
-      room: currentRoom,
-    }
-
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'POST',
-      data: JSON.stringify(ourPost),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  }
-  $('#messageBox').keypress(function(event){
-    // if enter key is pressed
-    if (event.keyCode === 13){
-      // post message
-      postMessage($('#messageBox').val());
+app.fetch = function(){
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    success: function (data) {
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
     }
   });
+};
 
-  getMessages();
-});
+app.clearMessages = function(){
+  $('#chats').children().remove();
+};
+
+app.addRoom = function(roomName){
+  var $option = $('<option>');
+  $option.attr('value', roomName);
+  $option.addClass('room');
+  $option.appendTo($('#roomSelect'));
+}
+
+// var message = {
+//  username: 'Mel Brooks',
+//  text: 'Never underestimate the power of the Schwartz!',
+//  roomname: 'lobby'
+// };
+app.addMessage = function(message){
+  var $post = $('<div></div>');
+  $post.addClass('post');
+  
+  var $username = $('<span></span>');
+  $username.text(message.username);
+  $username.addClass('username');
+  $username.appendTo($post);
+
+  var $roomname = $('<span></span>');
+  $roomname.text(message.roomname); 
+  $roomname.addClass('roomname');
+  $roomname.appendTo($post);
+
+  var $message = $("<p></p>");
+  $message.addClass('message');
+  $message.text(message.text);
+  $message.appendTo($post);
+  
+  $post.appendTo($("#chats"));
+};
+
+app.addFriend = function(){
+  console.log('added friend');
+};
+
+app.handleSubmit = function(){
+  console.log('submitted');
+};
+
+// $(document).ready(function(){
+//   var rooms = {};
+//   var currentRoom = "default";
+//   var getMessages = function(){
+//     $.ajax({
+//       // This is the url you should use to communicate with the parse API server.
+//       url: 'https://api.parse.com/1/classes/chatterbox',
+//       type: 'GET',
+//       success: function (data) {
+//         console.log(data);
+//         for(var i = 0; i < data.results.length; i++){
+//             var item = data.results[i];
+//             rooms[item.roomname] = item.roomname;
+          
+//           if(item.roomname === currentRoom || currentRoom === "default"){
+//             var $post = $('<div></div>');
+//             $post.addClass('post');
+            
+//             var $username = $('<span></span>');
+//             $username.text(item.username);
+//             $username.addClass('username');
+//             $username.appendTo($post);
+            
+//             var $time = $('<span></span>');
+//             $time.text(item.createdAt); 
+//             $time.addClass('time');
+//             $time.appendTo($post);
+            
+//             var $roomname = $('<span></span>');
+//             $roomname.text(item.roomname); 
+//             $roomname.addClass('roomname');
+//             $roomname.appendTo($post);
+
+//             var $message = $("<p></p>");
+//             $message.addClass('message');
+//             $message.text(item.text);
+//             $message.appendTo($post);
+            
+//             $post.appendTo($("#chats"));
+//           }
+//         }
+//         // remove rooms from datalist
+//         $('.room').remove();
+
+//         // add rooms to datalist
+//         for(var room in rooms){
+//           var $option = $('<option>');
+//           $option.attr('value', room);
+//           $option.addClass('room');
+//           $option.appendTo($('datalist#roomList'));
+//         }
+//       },
+//       error: function (data) {
+//         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//         console.error('chatterbox: Failed to send message');
+//       }
+//     });
+//   };
+//   $('#new').on('click', function(){
+//     // select all posts and remove them
+//     $('.post').remove();
+//     getMessages();
+//   });
+
+//   $('#myRoom').on('input',function(e){
+//     currentRoom = $(this).val();
+
+//     // remove rooms from datalist
+//     console.log(currentRoom);
+//     $('.room').remove();
+
+//     // add rooms to datalist
+//     for(var room in rooms){
+//       var $option = $('<option>');
+//       $option.attr('value', room);
+//       $option.addClass('room');
+//       $option.appendTo($('datalist#roomList'));
+//     }
+
+//     //update messages for room
+//     getMessages();
+//   });
+
+//   var postMessage = function(message){
+//     var ourPost = {
+//       username: window.location.search.slice(10),
+//       text: message,
+//       room: currentRoom,
+//     }
+
+//     $.ajax({
+//       // This is the url you should use to communicate with the parse API server.
+//       url: 'https://api.parse.com/1/classes/chatterbox',
+//       type: 'POST',
+//       data: JSON.stringify(ourPost),
+//       contentType: 'application/json',
+//       success: function (data) {
+//         console.log('chatterbox: Message sent');
+//       },
+//       error: function (data) {
+//         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//         console.error('chatterbox: Failed to send message');
+//       }
+//     });
+//   }
+//   $('#messageBox').keypress(function(event){
+//     // if enter key is pressed
+//     if (event.keyCode === 13){
+//       // post message
+//       postMessage($('#messageBox').val());
+//     }
+//   });
+
+//   getMessages();
+// });
